71ca88a4ae7fd21d2582b7a43cb573ddf7bf6233
pelazas <carlospelazas@gmail.com>
2026-02-10 16:24:27 +0100

    binary tree level order traversal

---
+++ dfs-bfs/binary-tree-level-order-traversal/result.py
@@ -0,0 +1,31 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if not root:
+            return []
+        res = []
+        queue = deque([(root, 0)]) #store (node,level) nodes
+
+        while queue:
+            (node, level) = queue.popleft()
+
+            if level == len(res):
+                res.append([])
+            
+            res[level].append(node.val)
+
+            if node.left:
+                queue.append((node.left, level+1))
+            if node.right:
+                queue.append((node.right, level+1))
+        
+        return res
+
+
+        
+        
